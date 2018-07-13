d73a25db3ecd2ce73a7297fd99e952886af32ab6
at2264402 <atidd@student.rccd.edu>
2018-07-12 21:35:37 -0700

    Signed-off-by: at2264402 <atidd@student.rccd.edu>

---
+++ Hmwk/Lab6_MenuForAssignment4/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=iamsodonewiththis_
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/iamsodonewiththis_
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=iamsodonewiththis!.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/iamsodonewiththis!.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=iamsodonewiththis_
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/iamsodonewiththis_
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=iamsodonewiththis!.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/iamsodonewiththis!.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
